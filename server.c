5cf2fca914bb6e028e9498dbda1539b591ef2a7f
Anton <113907270+s224268@users.noreply.github.com>
2023-08-31 12:03:13 +0200

    is it getting closer to working? eeeeh

--- server.c
+++ server.c
@@ -2,16 +2,27 @@
 #include <string.h>
 #include "include/openssl/sha.h"
 
-uint64_t match(unsigned char hash[32], uint64_t start, uint64_t end){
+uint64_t match(uint8_t hash[32], uint64_t start, uint64_t end){
 
-    for (; start<=end; start++){
-        unsigned char guess[32];
-        SHA256(hash, strlen(hash), guess);
-        if (strcmp(guess, hash) == 0){
+    int j;
+    for (j = 0; j < 32; j++) {
+        printf("%02x", hash[j]);
+    }
+    printf("\n");
+
+    for(;start<=end; start++){
+        char hexstring[16];
+        sprintf(hexstring, "%llX", start);
+        char guess[32];//Shouldnt this be 16?
+
+       // unsigned char hexstring[32];
+        SHA256(hexstring, strlen(hexstring), guess);
+        //printf("%s", hash);
+        if (strcmp(hexstring, hash) == 0){
+            printf("%s", "boobs");
             printf("%llu", start);
             return start;
         }
-
     }
     return 1;
 }
@@ -22,7 +33,7 @@ int main()
 
     char hexstring[16];
     unsigned char hash[32];
-    sprintf(hexstring, "%X", correct);
+    sprintf(hexstring, "%llX", correct);
 
     printf("%s\n",hexstring);
 
